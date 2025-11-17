2c369ad51753af68b7af5d3602459306896d6bec
Arnika <arnika21sh@gmail.com>
2025-11-17 18:20:12 +0530

    inheritance

---
+++ Inheritance/Domain/Customer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance.Domain
+{
+     class Customer
+    {
+        private string _id;
+        private string _name;
+        private string _location;
+
+
+        public Customer(string id,string name):this(id,name,"Noida")//we are envoking the constructor
+        {
+
+        }
+        public Customer(string id,string name,string location)
+        {
+            _id = id;
+            _name = name;
+            this._location = location;//showing instance _location is changed as per the location
+        }public string id
+        {
+            get { return _id; }
+        }
+        public string name
+        {
+            get { return _name; }
+        }
+        public string location
+        {
+            get { return _location; }
+        }
+        public override string ToString()
+        {
+            return $"id: {_id} , name: {_name} , location:{_location} ";
+        }
+        public override bool Equals(object second)
+        {
+            Customer c=second as Customer;
+            return(this._id==c._id);
+        }
+    }
+}
---
+++ Inheritance/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using Inheritance.Domain;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main()
+        {
+           Customer c1=new Customer("C2088", "Arnika","Kanpur");
+            Console.WriteLine(c1);
+            //Console.WriteLine("Object Created");
+            Console.WriteLine(c1.ToString());
+            Console.WriteLine(c1.GetType());
+            // Printinfo(new Customer("C2110", "Arnika"));
+            Customer C2 = new Customer("C376", "Vidushi", "Delhi");
+            Console.WriteLine(c1==C2);//Reference Equality checks for stack value and this is different
+            Console.WriteLine(c1.Equals(C2));//now this is overidden by equal function
+
+
+        }
+        //static void CaseStudy1()
+        //{
+        //    Printinfo(new Customer("C2110", "Arnika"));
+        //    Printinfo(new Customer("C3728", "Divi", "Noida"));
+        //}
+        static void Printinfo(Customer customer)
+        {
+            Console.WriteLine("id: "+customer.id);
+            Console.WriteLine(customer.name);
+            Console.WriteLine(customer.location);
+        }
+    }
+}
